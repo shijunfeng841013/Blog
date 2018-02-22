d96ed52c5084eb0bcef8f9a472f29d5ee0e31a0a
shijunfeng841013 <johnshiluck@gmail.comgit>
2018-02-22 20:51:36 +0800

    styling and structure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,26 @@ class PostsController < ApplicationController
     @post.save
     redirect_to @post
   end
+  def edit
+		@post = Post.find(params[:id])
+	end
+
+  def update
+		@post = Post.find(params[:id])
+
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+  def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+ 	end
 
   private
 
